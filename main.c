45976bc664341ec7d9001a03535336e5b7a4346d
hemmpd <heidi.rytkonen@outlook.com>
2018-07-06 21:22:37 +0300

    modified main.c

--- main.c
+++ main.c
@@ -1,6 +1,11 @@
-#include <stdio>
+#include <stdio.h>
+#include "glauber.h"
 
 int main(int argc, char *argv[]) {
+
+  double gl;
+  gl = woods_saxon(1, 2, 1, 2, 1);
+  printf("Glauber gives %f\n", gl);
 
   return 0;
 }
